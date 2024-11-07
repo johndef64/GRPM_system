98038ac7a1b606f6d94bd4924de4e8bdc9dd59db
johndef64 <116732521+johndef64@users.noreply.github.com>
2024-11-07 14:11:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pygrpm',
-    version='0.1',
+    #version='0.1',
     packages=find_packages(),  # Automatically find packages in your project
     install_requires=[
         'os',
