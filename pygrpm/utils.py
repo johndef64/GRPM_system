c5f45fbb66358dfbf847b6083d10a6e110ae89e0
johndef64 <116732521+johndef64@users.noreply.github.com>
2025-02-16 20:45:43 +0100

    update

--- pygrpm/utils.py
+++ pygrpm/utils.py
@@ -1,11 +1,25 @@
 
 import importlib
 import subprocess
+import requests
+import os
 
 def simple_bool(message):
     choose = input(message+" (y/n): ").lower()
     your_bool = choose in ["y", "yes"]
     return your_bool
+
+
+def get_file(url, file_name, dir = os.getcwd()):
+    url = url
+    file_name = file_name
+    response = requests.get(url)
+    if response.status_code == 200:
+        content = response.content
+        file_path = os.path.join(dir, file_name)
+        with open(file_path, 'wb') as file:
+            file.write(content)
+
 
 def check_and_install_requirements(requirements: list):
     missing_requirements = []
